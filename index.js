c937741aa0ea25bcd7d927a79a293f4414c7dd61
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-11-27 19:06:37 +0600

    delete user from database

--- index.js
+++ index.js
@@ -93,6 +93,13 @@ async function run() {
 			const result = await userCollection.insertOne(user);
 			res.send(result);
 		});
+
+		app.delete("/user/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await userCollection.deleteOne(query);
+			res.send(result);
+		});
 	} finally {
 		// Ensures that the client will close when you finish/error
 		//   await client.close();
